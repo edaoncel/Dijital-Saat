52a0fb9c99f3b2f60eb7ae166d74387109341981
eda oncel <edaoncel@email.com>
2023-05-19 14:23:38 +0300

    Dijital Saat Projesi

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace dijital_saat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.tmrSaat = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSaat.Location = new System.Drawing.Point(356, 164);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(27, 20);
+            this.lblSaat.TabIndex = 0;
+            this.lblSaat.Text = "[--]";
+            this.lblSaat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Location = new System.Drawing.Point(620, 34);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(106, 40);
+            this.btnKapat.TabIndex = 1;
+            this.btnKapat.Text = "[X]";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // tmrSaat
+            // 
+            this.tmrSaat.Enabled = true;
+            this.tmrSaat.Interval = 1000;
+            this.tmrSaat.Tick += new System.EventHandler(this.tmrSaat_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.lblSaat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.Timer tmrSaat;
+    }
+}
+
